1c65ba325435a7526c70d3e6d05fa564b5298475
jtb <james.baker@vanderbilt.edu>
2022-02-08 13:39:29 -0600

    continued working on the find_networks method so that it creates the list of pair objects, teh list of individuals in the network, and the list of haplotypes in the network.

--- IBDCluster/models/cluster_class.py
+++ IBDCluster/models/cluster_class.py
@@ -1,12 +1,14 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 import pandas as pd
-from typing import Dict, List
-from models import Pairs
+from typing import Dict, List, Set
+from models import Pairs, File_Info
 import os
 
 @dataclass
 class Cluster:
+    gene_name: str
     ibd_file: str
+    individuals_in_network: Set[str] = field(default_factory=set)
     network_id: int = 1
 
     def load_file(self, start: int, end: int, start_indx: int, end_indx:int) -> None:
@@ -50,14 +52,129 @@ class Cluster:
         if os.environ.get("verbose", "False") == "True":
             print(f"Found {self.ibd_df.shape[0]} pairs that have matching haplotypes")
 
-    def find_networks(self) -> Dict[int: Dict]:
+    @staticmethod
+    def _find_all_grids(dataframe: pd.DataFrame, ind_1_indx: int, ind_2_indx: int) -> List[str]:
+        """Method that will take the dataframe that is filtered for the location and the haplotype and return 
+        a list of all unique individuals in that dataframe
+        
+        Parameters
+        
+        dataframe : pd.DataFrame
+            dataframe that has the pairs of individuals who share an ibd segment. This dataframe is filtered for a 
+            specific loci and matching haplotype phase.
+
+        ind_1_indx : int
+            integer for the column that has the individual 1 IIDs
+
+        ind_2_indx : int
+            integer for the column that has the individual 2 IIDs
+            
+        Returns
+        
+        List[str]
+            returns a list of unique individuals in the dataframe
+        """
+        return list(set(dataframe[ind_1_indx].values.tolist()+dataframe[ind_2_indx].values.tolist()))
+
+    @staticmethod
+    def _determine_pairs(ibd_row: pd.Series, indices: File_Info) -> Pairs:
+        """Method that will take each row of the dataframe and convert it into a pair object"""
+
+        return Pairs(
+            ibd_row[indices.id1_indx],
+            ibd_row[indices.id1_phase_indx],
+            ibd_row[indices.id2_indx],
+            ibd_row[indices.id2_phase_indx],
+            ibd_row[indices.chr_indx],
+            ibd_row[indices.str_indx],
+            ibd_row[indices.end_indx],
+            ibd_row[indices.cM_indx]
+            )
+    
+    @staticmethod
+    def _determine_iids_in_network(dataframe: pd.DataFrame, indices: File_Info) -> List[str]:
+        """Staticmethod that will find all the unique individuals in the dataframe and return that list
+        
+        Parameters
+        
+        dataframe : pd.DataFrame
+            dataframe that has all the ibd sharing for pairs of individuals
+            
+        indices : Union[Hapibd_Info | Ilash_Info]
+            class object that has the indices for either hapibd files or ilash files
+        
+        Returns
+
+        List[str]
+            returns a list of unique iids that are in the dataframe
+        """
+        return list(set(dataframe[indices.id1_indx].values.tolist()+dataframe[indices.id2_indx].values.tolist()))
+
+    @staticmethod
+    def _gather_haplotypes(dataframe: pd.DataFrame, indices: File_Info) -> List[str]:
+        """Static Method that will determine the unique haplotypes within the pairs
+        
+        Parameters
+        
+        dataframe : pd.DataFrame
+            dataframe that has all the ibd sharing for pairs of individuals
+            
+        indices : Union[Hapibd_Info | Ilash_Info]
+            class object that has the indices for either hapibd files or ilash files
+        
+        Returns
+
+        List[str]
+            returns a list of unique haplotypes that are in the dataframe
+        """
+        pair_1_haplotypes: List[str] = list(set(list(dataframe[indices.id1_indx] + "." + dataframe[indices.id1_phase_indx])))
+
+        pair_2_haplotypes: List[str] = list(set(list(dataframe[indices.id2_indx] + "." + dataframe[indices.id2_phase_indx])))
+
+        return list(set(pair_1_haplotypes+pair_2_haplotypes))
+
+    def find_networks(self, indices: File_Info) -> Dict[int: Dict]:
         """Method that will go through the dataframe and will identify networks.
         
         Parameters
         
-        
+        ind_1_indx : int
+            integer for the column that has the individual 1 IIDs
+
+        ind_2_indx : int
+            integer for the column that has the individual 2 IIDs
+
         Returns
+
         Dict[int: Dict]
             returns a dictionary with the following structure:
             {network_id: {pairs: List[Pair_objects], in_network: List[str], haplotypes_list: List[str]}}. Other data information will be added to this file
         """
+        # First need to get a list of all the unique individuals in the network
+        iid_list: List[str] = self._find_all_grids(self.ibd_df, indices.id1_indx, indices.id2_indx)
+
+        # creating a dictionary that will return information of interest
+        network_info: Dict[int: Dict] = {}
+
+        # iterate over each iid in the original dataframe
+        for ind in iid_list:
+
+            # creating a key in the dictionary for the network id
+            network_info[self.network_id] = {}
+
+            # if this iid has already been associated with a network then we need to skip it. If not then we can get the network connected to it
+            if ind not in self.individuals_in_network:
+                
+                filtered_df: pd.DataFrame = self.ibd_df[(self.ibd_df[indices.id1_indx] == ind) | (self.ibd_df[indices.id2_indx] == ind)]
+
+                # forming a list of Pair objects that will be added to the network_info_dict in a pairs key
+                network_info[self.network_id]["pairs"] = list(filtered_df.apply(lambda row: self._determine_pairs(row, indices), axis=1))
+
+                # need to create a list of people in the network
+                network_info[self.network_id]["in_network"] = self._determine_iids_in_network(filtered_df, indices)
+
+                # now need to get a list of haplotypes 
+
+
+
+
