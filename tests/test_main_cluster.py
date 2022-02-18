fa8546dacf178703f4830800edd9e19fdc307207
jtb <james.baker@vanderbilt.edu>
2022-02-18 10:06:18 -0600

    started to build the unittest framework. The first test checks to make sure the returned gene tuple has the correct fields. This was the only way to test if the Genes type object was being returned

---
+++ tests/test_main_cluster.py
@@ -0,0 +1,18 @@
+import sys
+from collections import namedtuple
+import logging
+sys.path.append("../IBDCluster")
+from cluster.main import load_gene_info
+
+def test_loading_gene_info_return() -> None:
+    """Function to make sure that the return object is of type Genes"""
+    filepath = "./test_data/HK2_gene_info_1_31_22.txt"
+
+    gene_generator = load_gene_info(filepath)
+
+    gene_tuple = list(gene_generator)[0]
+
+    print(gene_tuple._asdict())
+    assert all(field in gene_tuple._asdict().keys() for field in ["name", "chr", "start", "end"]), "expected the gene_tuple to have the fields 'name', 'chr', 'start', 'end'"
+
+
