3e5bef47b99af992404f654fbebfd0bd71d33922
jtb <james.baker@vanderbilt.edu>
2022-03-02 13:12:36 -0600

    changed the indices.py to prefer composition where the FileInfo class is composed on the individual IBD classes depending on what the user supplies. Also created a protocol, ProgramIndices that insures that the individual IBD classes implement the gather_files method

--- IBDCluster/models/indices.py
+++ IBDCluster/models/indices.py
@@ -1,13 +1,60 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 import os
-from typing import List, Optional
+from typing import List, Optional, Protocol
 from glob import glob
 import log
 
 logger = log.get_logger(__name__)
 
+class ProgramIndices(Protocol):
+    """Interface indicating that the class has to have a gather files method"""
+    file_dir: Optional[str]
+    cM_indx: int
+    
+    def gather_files(self) -> List[str]:
+        """Method to gather a list of output files for the correct ibd program"""
+        ...
+
 @dataclass
-class File_Info:
+class HapibdInfo(ProgramIndices):
+    """Class that has all of the indices as well as file paths for the hapibd files."""
+    file_dir: Optional[str] = None
+    cM_indx: int = 7
+
+    def gather_files(self) -> List[str]:
+        """Function that will gather together all the files in the file_dir that have the ext and will store them in a list"""
+
+        file_list = []
+
+        for file in glob(os.path.join(self.file_dir, "*.ibd.gz")):
+
+            file: str = os.path.join(self.file_dir, file)
+
+            file_list.append(file)
+    
+        return file_list
+
+@dataclass
+class IlashInfo(ProgramIndices):
+    """Class that has all of the indices as well as file paths for the ilash files."""
+    file_dir: Optional[str] = None
+    cM_indx: int = 9
+
+    def gather_files(self) -> None:
+        """Function that will gather together all the files in the file_dir that have the ext and will store them in a list"""
+
+        file_list = []
+
+        for file in glob(os.path.join(self.file_dir, "*.match.gz")):
+
+            file: str = os.path.join(self.file_dir, file)
+
+            file_list.append(file)
+    
+        return file_list
+
+@dataclass
+class FileInfo:
     """The child classes will have attributes such as:
             'id1_indx': 0,
             'id2_indx': 2,
@@ -22,18 +69,26 @@ class File_Info:
     chr_indx: int = 4
     str_indx: int = 5
     end_indx: int = 6
+    ibd_files: List[Optional[str]] = field(default_factory=list)
+    program_indices: Optional[ProgramIndices] = None
 
-    def gather_files(self) -> None:
-        """Function that will gather together all the files in the file_dir that have the ext and will store them in a list"""
+    def set_program_indices(self, program_name: str) -> None:
+        """Method that will set the program indices as either Hapibd_Info or Ilash_Info
+        
+        Parameters
+        
+        program_name : str
+            This will be either ilash or hapibd
+        
+        filepath : str
+            string to the directory with ibd files
+        """ 
+        if program_name == "hapibd":
+            self.program_indices = HapibdInfo("/data100t1/share/BioVU/shapeit4/Eur_70k/hapibd/")
+        else:
+            self.program_indices = IlashInfo("/data100t1/share/BioVU/shapeit4/Eur_70k/iLash/min100gmap/")
 
-        self.ibd_files: List[str] = []
-        for file in glob(os.path.join(self.file_dir, self.ext)):
-
-            file: str = os.path.join(self.file_dir, file)
-
-            self.ibd_files.append(file)
-
-    def find_file(self, chr_num: str) -> str:
+    def find_file(self, chr_num: str, file_list: List[str]) -> str:
         """Method that will find the appropriate file based on the correct chromosome number
         
         Parameters
@@ -41,32 +96,26 @@ class File_Info:
         chr_num : str
             identifier for the chromosome number this wilk take the format 'chrX' or 'chrXX' depending on 
             whether the X is < 10 or >= 10
+        
+        file_list : List[str]
+            list of filepaths to the ibd files of interest
 
         Returns
 
         str
             returns the file as a string
         """ 
-        ibd_file: str = [file for file in self.ibd_files if "".join([chr_num, "."]) in file][0]
+        try:
+            ibd_file: str = [file for file in file_list if "".join([chr_num, "."]) in file][0]
+        except IndexError as traceback:
+            logger.critical(f"Attempts to find chromosome, {chr_num} in file list, {', '.join(file_list)}, resulted in the following index out of range error:")
+            logger.critical(traceback, exc_info=1)
 
         logger.debug(f"Found the ibd file, {ibd_file}, that matches the chromosome, chr_num")
         
         return ibd_file
 
-@dataclass
-class Hapibd_Info(File_Info):
-    """Class that has all of the indices as well as file paths for the hapibd files."""
-    file_dir: Optional[str] = None
-    ext: str = "*.ibd.gz"
-    cM_indx: int = 7
-    
 
-@dataclass
-class Ilash_Info(File_Info):
-    """Class that has all of the indices as well as file paths for the ilash files."""
-    file_dir: Optional[str] = None
-    ext: str = "*.match.gz"
-    cM_indx: int = 9
 
     
         
