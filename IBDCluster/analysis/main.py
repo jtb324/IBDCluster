8a988d29f71782ef0bbb1673baf4c59af1e7f18d
jtb <james.baker@vanderbilt.edu>
2022-03-03 15:58:08 -0600

    removed unnecessary code that was used for determine the carrier status for each pair. Also formated the files

--- IBDCluster/analysis/main.py
+++ IBDCluster/analysis/main.py
@@ -2,9 +2,9 @@ from typing import Protocol, Dict, Tuple, List, Set
 from collections import namedtuple
 import pandas as pd
 from scipy.stats import binom
+from tqdm import tqdm
 from models import PairWriter, NetworkWriter
 import analysis
-from tqdm import tqdm
 import log
 
 logger = log.get_logger(__name__)
@@ -14,6 +14,7 @@ class CarriersInfo(
     namedtuple("Carrier_Comp", ["ind_in_network", "percentage", "IIDs", "pvalue"])
 ):
     """This is an extension of the class tuple so that I can overwrite the __str__ method"""
+
     def __str__(self):
         return f"Carrier Info Object - Individuals in Network: {self.ind_in_network}, Percentages: {self.percentage}, IID List: {self.IIDs}, pvalue: {self.pvalue}"
 
@@ -33,30 +34,11 @@ class Writer(Protocol):
     and a set_writer method"""
 
     def set_writer(self, writer: WriteObject) -> None:
+        """Method that determine the writer for the appropriate output"""
         ...
 
     def write_to_file(self, output: str, networks_dict: Dict) -> None:
         ...
-
-
-def _generate_carrier_list(carriers_matrix: pd.DataFrame) -> Dict[str, List[str]]:
-    """Function that will take the carriers_pheno_matrix and generate a dictionary that has the list of carriers for each phenotype"""
-    return_dict = {}
-
-    # iterating over each phenotype which starts with the
-    # second column
-
-    for column in carriers_matrix.columns[1:]:
-
-        filtered_matrix: pd.DataFrame = carriers_matrix[carriers_matrix[column] == 1][
-            ["grids", column]
-        ]
-
-        return_dict[column] = filtered_matrix.grids.values.tolist()
-
-    logger.debug(f"identified carriers for {len(return_dict.keys())} phenotypes")
-
-    return return_dict
 
 
 def determine_pvalue(
@@ -78,13 +60,12 @@ def determine_pvalue(
         return 1
 
     prob: float = 1 - binom.cdf(
-            carriers_count - 1, network_size, percentage_pop_phenotypes[phenotype]
-        )
+        carriers_count - 1, network_size, percentage_pop_phenotypes[phenotype]
+    )
 
     return prob
 
 
-# FIXME: This needs to be refactored. The function is doing many different things
 def determine_in_networks(
     carriers_list: Dict[str, List[str]],
     info_dict: Dict[int, Dict],
@@ -137,96 +118,16 @@ def determine_in_networks(
         info["phenotype"] = phenotype_info
 
 
-def _determine_pair_carrier_status(
-    phenotype_list: List[str], carrier_list: Dict[str, List[str]], pair_id: str
-) -> str:
-    """Function that will iterate over the phenotype list and get a list of carriers for each one and check if the pair id is in each list
-
-    Parameters
-
-    phenotype_list : List[str]
-        List of phenotypes. This gives us a consistent order for the columns in
-        the file
-
-    carrier_list : Dict[str, List[str]]
-        Dictionary that has the phenotypes as keys and list of IIDs that carry that phenotype as values
-
-    pair_id : str
-        id of the pair of interest
-
-    Returns
-
-    str
-        returns a string with either 0's or 1's for whether the individual is a carrier or not. These values will be tab separated
-    """
-    # iterrating over each phenotype to get the carriers
-    carrier_str: str = ""
-
-    for phenotype in phenotype_list:
-
-        carriers: List[str] = carrier_list[phenotype]
-
-        if pair_id in carriers:
-            logger.debug(f"id {pair_id} is in the list carriers for phenotype {phenotype}")
-            carrier_str += "1\t"
-
-        else:
-            logger.debug(f"id {pair_id} is not in the list of carriers for phenotype {phenotype}")
-            carrier_str += "0\t"
-
-    carrier_str = carrier_str.strip("\t")
-
-    return carrier_str
-
-
-def analyze_pair_carrier_status(
-    phenotype_list: List[str],
-    carrier_list: Dict[str, List[str]],
-    info_dict: Dict[int, Dict],
-) -> None:
-    """Function that will iterate through each pair in a network and add the carrier status for each id for each phenotype
-
-    Parameters
-
-    phenotype_list : List[str]
-        List of phenotypes. This gives us a consistent order for the columns in
-        the file
-
-    carriers_list : dict[str, List[str]]
-        Dictionaary that has all the carriers in list for each phenotype of interest
-
-    info_dict : Dict[int, Dict]
-        dictionary where the keys are network ids and the inner dictionary has information about the network ind different keys
-    """
-    logger.info("determining the carrier status for each id for each phenotype")
-
-    for net_id, info in info_dict.items():
-
-        # getting all of the pair objects and iterate over them
-        pair_list = info["pairs"]
-
-        for pair in tqdm(pair_list, desc=f"Determine pair carrier status for network {net_id}"):
-            logger.debug(f"Determine carrier status for pair: {pair.pair_1}-{pair.pair_2}")
-            carrier_str_1: str = _determine_pair_carrier_status(
-                phenotype_list, carrier_list, pair.pair_1
-            )
-
-            carrier_str_2: str = _determine_pair_carrier_status(
-                phenotype_list, carrier_list, pair.pair_2
-            )
-
-            pair.carrier_str_1 = carrier_str_1
-
-            pair.carrier_str_2 = carrier_str_2
-
-
 def analyze(
     gene_info: Tuple[str, str],
     network_info: Dict[int, Dict],
     carriers_pheno_matrix: pd.DataFrame,
     writer: Writer,
+    carriers_lists: Dict[float, List[str]],
 ):
-    """Main function from the analyze module that will determine the pvalues and the number of haplotypes, and individuals and will write this all to file.
+    """Main function from the analyze module that will determine the
+    pvalues and the number of haplotypes, and individuals and will
+    write this all to file.
 
     Parameters
 
@@ -247,20 +148,22 @@ def analyze(
         object that will be used to write output to both the
         networks.txt file as well as the allpairs.txt file.
         This object follows the Writer protocol above
-    """
 
-    # generate list of carriers for each phenotype
-    carriers_lists: Dict[int, List[str]] = _generate_carrier_list(carriers_pheno_matrix)
+    carriers_list : Dict[float, List[str]]
+        Dictionary where the keys are the phecodes and the values
+        are list of IIDs affected with that phecode
+    """
 
     # getting the percentage of carriers for each phenotype per the input population
     percent_carriers_in_pop: Dict[str, float] = analysis.get_percentages(
         carriers_pheno_matrix
     )
+
     # recording this percentage to keep track of it. CONSIDER JUST
     # ADDING THIS AS ANOTHER CLASS WITHIN THE WRITER
     analysis.write_to_file(percent_carriers_in_pop, writer.output)
 
-    phenotype_list: List[str] = list(carriers_lists.keys())
+    phenotype_list: List[str] = carriers_pheno_matrix.columns[1:]
     # we will iterate over each network and basically
     # determine the number of carriers for each grid as well
     # as the percent
@@ -271,8 +174,8 @@ def analyze(
 
     writer.write_to_file(network_info)
 
-    # adding information to the pair about the carrier string
-    analyze_pair_carrier_status(phenotype_list, carriers_lists, network_info)
+    # # adding information to the pair about the carrier string
+    # analyze_pair_carrier_status(phenotype_list, carriers_lists, network_info)
 
     # creating the allpairs.txt file
     writer.set_writer(PairWriter(gene_info[0], gene_info[1], phenotype_list))
--- IBDCluster/cluster/__init__.py
+++ IBDCluster/cluster/__init__.py
@@ -1,1 +1,1 @@
-from .main import find_clusters
+from .main import find_clusters, generate_carrier_list
