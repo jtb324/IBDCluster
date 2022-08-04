d1bb4cc239a605ae0e863d0ad9e456c1a1056733
jtb <james.baker@vanderbilt.edu>
2022-08-04 13:41:50 -0500

    sorted the imports, added a pylint compliant docstring, and then removed unnecessary log statements

--- IBDCluster/analysis/main.py
+++ IBDCluster/analysis/main.py
@@ -1,10 +1,14 @@
-from typing import Dict, Any
-from models import DataHolder, Network
+"""
+Main module that is used to call the different analysis plugins. This module takes
+advantage of the factory to dynamically load the modules while also passing the 
+appropriate information to the modules
+"""
+import json
+import os
+
 import log
 import plugins
-import os
-import json
-
+from models import DataHolder, Network
 
 logger = log.get_logger(__name__)
 
@@ -30,7 +34,7 @@ def analyze(data_container: DataHolder, network_obj: Network, output: str) -> No
 
     # making sure that the output directory is created
     # This section will load in the analysis plugins
-    with open(os.environ.get("json_path")) as json_config:
+    with open(os.environ.get("json_path"), encoding="utf-8") as json_config:
 
         config = json.load(json_config)
 
@@ -38,15 +42,14 @@ def analyze(data_container: DataHolder, network_obj: Network, output: str) -> No
 
         analysis_plugins = [plugins.factory_create(item) for item in config["modules"]]
 
-        logger.info(
+        logger.debug(
             f"Using plugins: {', '.join([obj.name for obj in analysis_plugins])}"
         )
 
         # iterating over every plugin and then running the analyze and write method
         for analysis_obj in analysis_plugins:
 
-            logger.debug(analysis_obj.name)
-            logger.debug(f"output being used in analysis: {output}")
+            logger.debug(f"output path being used in analysis: {output}")
             analysis_obj.analyze(
                 data=data_container, network=network_obj, output=output
             )
