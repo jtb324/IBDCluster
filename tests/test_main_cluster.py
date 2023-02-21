6b2b660628a581769c1374a78f6a7afb451cc814
jtb <james.baker@vanderbilt.edu>
2023-02-21 10:45:07 -0600

    Created initial unit test for the load_gene_info method: --- 1. Created unit test for the cluster.main.load_gene_info method
    This method is responsible fo reading in information about the loci such
    as gene name, chromosome, start and end position from a gene info file
    provided by the user. The method returns a generator that yields a Genes
    object which is a namedTuple that has attributes for the information.
    Created a class, TestLociLoader, that has three methods:
    test_load_loci_info_no_sliding_window,
    test_values_of_sliding_windows_formed,
    test_number_of_sliding_windows_formed.
    
    * *test_load_loci_info_no_sliding_window*: test when the user doesn't
    use a sliding window. Checks to make sure the named tuple has the proper
    keys
    
    * *test_values_of_sliding_windows_formed*: test when the user uses a
    sliding window. Makes sure that the first window formed has the correct
    name, chromosome, start and end position
    
    * *test_number_of_sliding_windows_formed*: test the number of sliding
    windows formed for the range in the gene_info.txt file.

--- tests/test_main_cluster.py
+++ tests/test_main_cluster.py
@@ -1,21 +1,75 @@
 import sys
 from collections import namedtuple
 import logging
+import pytest
 
 sys.path.append("../IBDCluster")
-from cluster.main import load_gene_info
+from cluster.main import load_gene_info  # pylint: disable="wrong-import-position"
 
 
-def test_loading_gene_info_return() -> None:
-    """Function to make sure that the return object is of type Genes"""
-    filepath = "./test_data/HK2_gene_info_1_31_22.txt"
+class TestLociLoader:
+    @pytest.fixture
+    def loci_info(self, request):
+        """Fixture that will generate the gene info object"""
+        return load_gene_info(request.param[0], request.param[1])
 
-    gene_generator = load_gene_info(filepath)
+    @pytest.mark.parametrize(
+        "loci_info", [("./test_data/gene_info.txt", False)], indirect=True
+    )
+    def test_load_loci_info_no_sliding_window(self, loci_info) -> None:
+        """Function to make sure that the return object is of type Genes"""
 
-    gene_tuple = list(gene_generator)[0]
+        gene_generator = loci_info
 
-    print(gene_tuple._asdict())
-    assert all(
-        field in gene_tuple._asdict().keys()
-        for field in ["name", "chr", "start", "end"]
-    ), "expected the gene_tuple to have the fields 'name', 'chr', 'start', 'end'"
+        for gene_tuple in gene_generator:
+            assert all(
+                field in gene_tuple._asdict().keys()
+                for field in ["name", "chr", "start", "end"]
+            ), "expected the gene_tuple to have the fields 'name', 'chr', 'start', 'end'"
+
+    @pytest.mark.parametrize(
+        "loci_info", [("./test_data/gene_info.txt", True)], indirect=True
+    )
+    def test_values_of_sliding_windows_formed(self, loci_info) -> None:
+        """Method to check how many steps are made in the sliding window list"""
+        gene_generator = loci_info
+
+        first_window = next(gene_generator)
+
+        errors = []
+        if first_window.name != "TEST_GENE_2769662-2770662":
+            errors.append(
+                f"Expected the first window to have the name: TEST_GENE_2769662-2770662. Instead it had the name: {first_window.name}"
+            )
+
+        if first_window.chr != 2:
+            errors.append(
+                f"Expected the first window to have the chromosome number: 2. Instead it had the chromosome number: {first_window.chr}"
+            )
+
+        if first_window.start != 2769662:
+            errors.append(
+                f"Expected the first window to have the start position: 2769662. Instead it had the start position: {first_window.start}"
+            )
+
+        if first_window.end != 2770662:
+            errors.append(
+                f"Expected the first window to have the end position: 2770662. Instead it had the end position: {first_window.end}"
+            )
+
+        assert not errors, "errors occured:\n{}".format("\n".join(errors))
+
+    @pytest.mark.parametrize(
+        "loci_info", [("./test_data/gene_info.txt", True)], indirect=True
+    )
+    def test_number_of_sliding_windows_formed(self, loci_info) -> None:
+        gene_generator = loci_info
+
+        loci_of_interest_list = list(gene_generator)
+        assert (
+            len(loci_of_interest_list) == 397
+        ), f"Expected 397 sliding window steps to be formed. Instead {len(loci_of_interest_list)} were formed"
+
+
+# def test_loading_loci_info_sliding_window() -> None:
+#     """Unit test that makes sure that the program can properly create the sliding window. It calls the cluster.main.load_gene_info function"""
