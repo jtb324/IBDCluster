30a396a3852eba0c3ff271603ab5494b9b78b7ce
jtb <james.baker@vanderbilt.edu>
2023-03-28 09:40:41 -0500

    refactor(drive/callbacks.py): created callback function to make sure that the input ibd file exist
    Created a function called check_input_exists that will either return the input Path object or it will raise a FileNotFoundError if the file doesn't exists. It is passed as a callback to the typer option input and takes whatever the user passes for that option. Need to also check if this input file is gzipped at some point.

---
+++ drive/callbacks.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+
+def check_input_exists(ibd_input_file: Path) -> Path:
+    """Callback that will check that the input ibd file exists
+
+    Parameters
+    ----------
+    ibd_input_file : Path
+        Path object to the input ibd file. This file should be gzipped
+
+    Returns
+    -------
+    Path
+        returns the Path object if it exists
+
+    Raises
+    ------
+    FileNotFoundError
+    """
+    if ibd_input_file.exists():
+        return ibd_input_file
+    else:
+        raise FileNotFoundError(f"The file, {ibd_input_file}, was not found")
