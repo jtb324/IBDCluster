65469bdb7f766f3979e0199f296620b0a6f647d0
jtb <james.baker@vanderbilt.edu>
2023-04-04 11:23:41 -0500

    test: added kcne1 tests and unit tests
    Created a unit test and kcne1 specific test to test the new filter module. The unit test "test_ibd_file_not_found" test whether the FileNotFoundError is raised by the
    rogram. The kcne1 test test_filter_results test whether the resulting ibd_pd attribute of the filter_obj has the same number of IBD segments and the same number of uniq
    e haplotype ids identified as from the code used for the KCNE1 paper. This test uses files from test_inputs for the filtering and it uses the "ibdpd_KCNE1.txt" file as atrue positive test.

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -2,28 +2,41 @@ from typer.testing import CliRunner
 import pytest
 import sys
 
-sys.path.append("../drive")
+sys.path.append("./drive")
 
-from DRIVE import app
+from drive.drive import app
 
 runner = CliRunner()
+
+# @pytest.mark.integtest
+# def test_drive_full_run():
+#     assert 1==1
 
 
 @pytest.mark.integtest
 def test_drive_full_run():
+
     result = runner.invoke(
         app,
         [
             "-i",
-            "",
+            "./tests/test_inputs/biovu_longQT_EUR_chr21.ibd.gz",
             "-f",
             "hapIBD",
             "-t",
             "22:35818986-35884508",
             "-o",
-            "./test_output/integration_test_results",
+            "./tests/test_output/integration_test_results",
             "-m",
             "3",
         ],
     )
+    # result = runner.invoke(
+    #     app,
+    #     [
+    #         "--help"
+    #     ],
+    #     color=True
+    # )
+    # print(result)
     assert result.exit_code == 0
