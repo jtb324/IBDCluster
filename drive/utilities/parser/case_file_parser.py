dff15fc221e181644639522d95b9d71cf7865048
jtb <james.baker@vanderbilt.edu>
2023-04-12 11:07:27 -0500

    refactor(PhenotypeFileParser): added functionality to the parser so it can determine cases/controls/exclusions
    added a method called parse_cases_and_controls that reads through the input file and separates individuals into case/controls/exclusion lists. This method returns those list
    so that the user can later use them. Created unit test that makes sure that the parser raises the proper exceptions and that it identifies the right number of individuals based on the test data

--- drive/utilities/parser/case_file_parser.py
+++ drive/utilities/parser/case_file_parser.py
@@ -1,6 +1,8 @@
+"""Module to faciliate the user in parsing the phenotype file by incorporating multiple ecodings, separators, and by handling multiple errors."""
+
 import gzip
 from pathlib import Path
-from typing import Union, TypeVar
+from typing import List, Tuple, Union, TypeVar
 from logging import Logger
 import log
 
@@ -11,10 +13,10 @@ T = TypeVar("T", bound="PhenotypeFileParser")
 
 
 class PhenotypeFileParser:
+    """Parser used to read in the phenotype file. This will allow use to account for different delimiters in files as well as catch errors."""
+
     def __init__(self, filepath: Union[Path, str]) -> None:
-        """Parser that will be used to read in the phenotype file.
-        This will allow use to account for different delimiters in
-        files as well as catch errors
+        """Initialize the PhenotypeFileParser class.
 
         Parameters
         ----------
@@ -25,6 +27,11 @@ class PhenotypeFileParser:
         ------
         FileNotFoundError
         """
+        # creating list to keep track of cases and controls
+        self.case_list: List[str] = []
+        self.control_list: List[str] = []
+        self.exclusion_list: List[str] = []
+        # now we are going to try to create an attribute for the input file
         filepath = Path(filepath)
         if not filepath.exists():
             raise FileNotFoundError(f"The file {filepath} was not found")
@@ -32,9 +39,7 @@ class PhenotypeFileParser:
             self.file: Path = filepath
 
     def __enter__(self) -> T:
-        """__enter__ method that will be automatically called when the
-        parser is used with the 'with' context manager. Method will
-        determine the appropriate file type and open the file
+        """Open the input file. Method determines the appropriate file type and open the file. Method is called automatically by the context manager.
 
         Raises
         ------
@@ -62,5 +67,87 @@ class PhenotypeFileParser:
         return self
 
     def __exit__(self, exc_type, exc_value, traceback) -> None:
-        """method that will be called be the context manager to close the resource when it is not longer being used"""
+        """Close the resource when it is not longer being used. Used by the context manager."""
         self.opened_file.close()
+
+    @staticmethod
+    def _check_separator(line: str) -> str:
+        """Determine what the separator for the file should be.
+
+        Parameters
+        ----------
+        line : str
+            this is the first or second line of the phenotype file
+            depending on whether or not the file has a header.
+
+        Returns
+        -------
+        str
+            returns the separator as a string. At the moment, comma
+            separated, tab separated, and pipe separated are supported.
+
+        Raises
+        ------
+        ValueError
+            raises a value error if the method doesn't identify a
+            supported separator.
+        """
+        if len(line.split(",")) == 2:
+            return ","
+        elif len(line.split("\t")) == 2:
+            return "\t"
+        elif len(line.split("|")) == 2:
+            return "|"
+        else:
+            raise ValueError(
+                "The was no appropriate separator found for the file. Currently DRIVE supports: ',' or '\t' or '|'"
+            )
+
+    def _generate_case_control(self, status_list: List[str]) -> None:
+        """Add the individual to the appropriate case/ control/exclusion list.
+
+        Parameters
+        ----------
+        status_list : List[str]
+            list of strings where the first element is the individual id
+            and the second element is the status coded as either 0, 1, N/
+            A, NA, or -1.
+        """
+        print(status_list)
+        if status_list[1] == "1":
+            self.case_list.append(status_list[0])
+        elif status_list[1] == "0":
+            self.control_list.append(status_list[0])
+        elif status_list[1].lower() in ["na", "n/a", "-1"]:
+            self.exclusion_list.append(status_list[0])
+        else:
+            logger.warning(
+                f"The status for individual, {status_list[0]}, was not recognized. The status found in the file was {status_list[1]}. This individual will be added to the exclusion list but it is recommended that the user checks to ensure that this is not a typo in the phenotype file."
+            )
+            self.exclusion_list.append(status_list)
+
+    def parse_cases_and_controls(self) -> Tuple[List[str], List[str], List[str]]:
+        """Generate a list for cases, controls, and excluded individuals.
+
+        Returns
+        -------
+        Tuple[List[str], List[str], List[str]]
+            returns a tuple where the first element is a list of case
+            ids, second element is a list of control ids, and the final
+            element is a list of excluded individuals.
+        """
+        separator = ""
+
+        for line in self.opened_file:
+            # we need to first check if there is a header row
+            if "grid" in line.lower() or "grids" in line.lower():
+                continue
+            # we can then determine the separator for the file
+            if separator == "":
+                separator = PhenotypeFileParser._check_separator(line)
+            # Now we can split the file using that separator
+            split_line = line.strip("\n").split(separator)
+
+            self._generate_case_control(split_line)
+
+        return self.case_list, self.control_list, self.exclusion_list
--- tests/test_phenotype_file_parser.py
+++ tests/test_phenotype_file_parser.py
@@ -8,8 +8,53 @@ from drive.utilities.parser import PhenotypeFileParser
 
 @pytest.mark.unit
 def test_file_not_found() -> None:
-    """Unit test to make sure that the parser s raising a FileNotFoundError if the phenotype file doesn't exist"""
-    ...
+    """Test that the parsers raises a FileNotFoundError if the phenotype file doesn't exist."""
     with pytest.raises(FileNotFoundError):
         with PhenotypeFileParser("./NotRealFile.txt") as pheno_file:
             ...
+
+
+@pytest.mark.unit
+@pytest.mark.parametrize(
+    "test_str,expected", [("ID1\t1", "\t"), ("ID1,1", ","), ("ID1|0", "|")]
+)
+def test_check_separator(test_str: str, expected: str) -> None:
+    """Test that the method _check_separator to see if it returns proper separator."""
+
+    assert PhenotypeFileParser._check_separator(test_str) == expected
+
+
+@pytest.mark.unit
+@pytest.mark.parametrize("test_str", [("ID1\n1")])
+def test_check_separator_error(test_str: str) -> None:
+    """Test that the method _check_separator raises a ValueError if it cannot identify the separator."""
+    with pytest.raises(ValueError):
+        PhenotypeFileParser._check_separator(test_str)
+
+
+@pytest.mark.unit
+def test_case_control_exclusion_counts() -> None:
+    """Check that the PhenotypeFileParser identifies the correct case/control/exlcusion counts."""
+    with PhenotypeFileParser("./tests/test_inputs/test_phenotype_file.txt") as parser:
+        cases, controls, exclusions = parser.parse_cases_and_controls()
+
+        expected_case_counts = 8
+        expected_controls_counts = 84
+        expected_exclusion_counts = 8
+
+        error_list = []
+
+        if len(cases) != expected_case_counts:
+            error_list.append(
+                f"Expected parser to identify {expected_case_counts} cases. Instead {len(cases)} cases were identified."
+            )
+        if len(controls) != expected_controls_counts:
+            error_list.append(
+                f"Expected parser to identify {expected_controls_counts} cases. Instead {len(controls)} cases were identified"
+            )
+        if len(exclusions) != expected_exclusion_counts:
+            error_list.append(
+                f"Expected parser to identify {expected_exclusion_counts} cases. Instead {len(exclusions)} cases were identified"
+            )
+
+        assert not error_list, "errors occured:\n{}".format("\n".join(error_list))
