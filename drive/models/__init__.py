3e9200d816078e988a16c0a254c561c779c425d3
jtb <james.baker@vanderbilt.edu>
2023-05-04 14:55:31 -0500

    refactor: Created models for the Data class and the network class
    Created files within models for the Data class and the Network class. This change makes organization a little simplier. Also created interfaces within each model for structural typing to reduce cohesion.

--- drive/models/__init__.py
+++ drive/models/__init__.py
@@ -1,3 +1,5 @@
 from .choices import FormatTypes, LogLevel
 from .generate_indices import FileIndices, create_indices
+from .networks import Network, Network_Interface
 from .types import Filter, Genes
+from .data_container import Data, Data_Interface
