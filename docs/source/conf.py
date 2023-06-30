4df97ed1d745a6cf95588f284930cae8a4c616cd
James Baker <james.baker@vanderbilt.edu>
2023-06-30 10:29:21 -0400

    fixing the configuration path for the conf.py file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,6 +5,12 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import sys
+import os
+
+# sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../../drive/'))
+sys.path.insert(0, os.path.abspath('../'))
 
 project = 'DRIVE'
 copyright = '2023, James Baker, Hung-Hsin Chen, Jennifer Piper-Below, David Samuels'
