74dbcfa0e3eca274c2a2ec5916ce08f5a2deffaf
James Baker <james.baker@vanderbilt.edu>
2023-07-20 08:50:08 -0500

    docs(conf.py): black formatted the conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,40 +7,41 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 import os
 import sys
+
 # import drive
 
 
-project = 'DRIVE'
-copyright = '2023, James Baker, Hung-Hsin Chen, David Samuels, Jennifer Piper-Below'
-author = 'James Baker, Hung-Hsin Chen, David Samuels, Jennifer Piper-Below'
-release = '2.0.1'
+project = "DRIVE"
+copyright = "2023, James Baker, Hung-Hsin Chen, David Samuels, Jennifer Piper-Below"
+author = "James Baker, Hung-Hsin Chen, David Samuels, Jennifer Piper-Below"
+release = "2.0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-sys.path.insert(0, os.path.abspath('../../drive/'))
-sys.path.insert(0, os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath("../../drive/"))
+sys.path.insert(0, os.path.abspath("../../"))
 
 github_url = "https://github.com/belowlab/drive"
 
 extensions = [
-    'sphinx.ext.napoleon',
-    'sphinx_copybutton',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.viewcode',
+    "sphinx.ext.napoleon",
+    "sphinx_copybutton",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.viewcode",
     # 'sphinxcontrib_autodocgen'
 ]
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-master_doc = 'index'
+master_doc = "index"
 extensions = []
 
 html_theme = "sphinx_book_theme"
 html_theme_options = {
     "repository_url": "https://github.com/belowlab/drive",
     "use_repository_button": True,
-    "home_page_in_toc": True
+    "home_page_in_toc": True,
 }
 html_title = "DRIVE"
